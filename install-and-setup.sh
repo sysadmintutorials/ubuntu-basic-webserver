3b5a4d3c27959a9e5febc345f6ff83a89bcfcc23
David Rodriguez <github@sysadmintutorials.com>
2025-04-08 22:53:59 +1000

    Update install-and-setup.sh

--- install-and-setup.sh
+++ install-and-setup.sh
@@ -1,11 +1,13 @@
-bash <<EOF
+#!/bin/bash
+
+# Updates
 apt update && apt upgrade -y
 
+# Nginx Install
 apt install -y nginx
-
 systemctl start nginx
-
 systemctl enable nginx
 
+# Create Index.html
 echo "<h2>Welcome to $(hostname) : $(hostname -I | awk '{print $1}')</h2><p> I am a basic web server created by Sysadmintutorials, enjoy." | tee /var/www/html/index.html
-EOF
+
